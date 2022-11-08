561d2f9b34460e2d17e8d9a068c1d25a3c3d0607
EngineerSahil <77582744+EngineerSahil@users.noreply.github.com>
2022-11-08 21:35:44 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+
+import './App.css';
+import { Feature } from './components/Feature';
+import { Footer } from './components/Footer';
+import { Header } from './components/Header';
+import { Nav } from './components/Nav';
+import { Team } from './components/Teams';
+
+
+
+function App() {
+  return (
+    <div className='max-w-screen-xl mx-auto'>
+      <Nav/>
+      <Header/>
+      <Feature/>
+      <Team/>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
