2b6b054cfb1ea65dc122a2647758186283098afd
EngineerSahil <77582744+EngineerSahil@users.noreply.github.com>
2022-11-08 21:58:25 +0600

    router setup

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 
+import { RouterProvider } from 'react-router-dom';
 import './App.css';
 import { Feature } from './components/Feature';
 import { Footer } from './components/Footer';
 import { Header } from './components/Header';
 import { Nav } from './components/Nav';
 import { Team } from './components/Teams';
+import router from './Router/Routes/Routes';
 
 
 
 function App() {
   return (
     <div className='max-w-screen-xl mx-auto'>
+      <RouterProvider router={router}></RouterProvider>
       <Nav/>
       <Header/>
       <Feature/>
